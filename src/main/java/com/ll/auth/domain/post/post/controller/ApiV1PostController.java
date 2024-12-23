559606263637d06038e18c56c6897a71b3917183
paek1127 <paek1127@gmail.com>
2024-12-23 11:38:43 +0900

    인증 header로 변경

--- src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
@@ -9,7 +9,6 @@ import com.ll.auth.global.exceptions.ServiceException;
 import com.ll.auth.global.rsData.RsData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.transaction.annotation.Transactional;
@@ -77,12 +76,7 @@ public class ApiV1PostController {
             String title,
             @NotBlank
             @Length(min = 2)
-            String content,
-            @NotNull
-            Long authorId,
-            @NotNull
-            @Length(min = 4)
-            String password
+            String content
     ) {
     }
 
@@ -90,11 +84,13 @@ public class ApiV1PostController {
     @Transactional
     public RsData<PostDto> modifyItem(
             @PathVariable long id,
-            @RequestBody @Valid PostModifyReqBody reqBody
+            @RequestBody @Valid PostModifyReqBody reqBody,
+            @RequestHeader long actorId,
+            @RequestHeader String actorPassword
     ) {
-        Member actor = memberService.findById(reqBody.authorId).get();
+        Member actor = memberService.findById(actorId).get();
 
-        if (!actor.getPassword().equals(reqBody.password)) {
+        if (!actor.getPassword().equals(actorPassword)) {
             throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
         }
 
@@ -120,22 +116,19 @@ public class ApiV1PostController {
             String title,
             @NotBlank
             @Length(min = 2)
-            String content,
-            @NotNull
-            Long authorId,
-            @NotNull
-            @Length(min = 4)
-            String password
+            String content
     ) {
     }
 
     @PostMapping
     public RsData<PostDto> writeItem(
-            @RequestBody @Valid PostWriteReqBody reqBody
+            @RequestBody @Valid PostWriteReqBody reqBody,
+            @RequestHeader long actorId,
+            @RequestHeader String actorPassword
     ) {
-        Member actor = memberService.findById(reqBody.authorId).get();
+        Member actor = memberService.findById(actorId).get();
 
-        if (!actor.getPassword().equals(reqBody.password)) {
+        if (!actor.getPassword().equals(actorPassword)) {
             throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
         }
 
