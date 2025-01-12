70b93184dcd7d994ee288e5b0419c36e9104f7fe
paek1127 <paek1127@gmail.com>
2025-01-12 19:12:02 +0900

    postcomment write 추가

---
+++ src/main/java/com/ll/auth/standard/util/Ut.java
@@ -0,0 +1,10 @@
+package com.ll.auth.standard.util;
+
+public class Ut {
+    public static class str {
+
+        public static boolean isBlank(String str) {
+            return str == null || str.trim().isEmpty();
+        }
+    };
+}
